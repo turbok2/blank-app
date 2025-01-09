0faa3cb3306016546b5ec56ca82673d1da35cc05
Eric Yang <turbok2@gmail.com>
2025-01-09 12:48:05 +0000

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("내가 만든 앱")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.markdown("""
+sjdhfgs sdlgkjhdlsjgs
+g
+sdg
+sdgsdgnsldgjlsdgsd
+            gsgs
+
+            """)
